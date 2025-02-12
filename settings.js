eacede2e12f5c003ec6d8b5928ba6d0c83186050
TH3 F9M0US B0Y ARAV H3R3 <153765611+rahilpanchal@users.noreply.github.com>
2025-02-12 19:35:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'MALAKA-MD~6IAlGQoR#Qqe7QdYkxXCPF2ZfbTBjWE08WaCdmgU6ki_tI4s_xzk' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'MALAKA-MD~vIwTSQab#0-4tNWuA3_FpqMn_G0tCWOLQ_ozQmxanD74hjZn7gVw' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS === undefined ?"true" : process.env.AUTO_REACT_STATUS,
